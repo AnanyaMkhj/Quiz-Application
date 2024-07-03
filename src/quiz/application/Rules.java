5ae59c24c2ebc086790231b0cbd9297dddd51971
Ananya Mukherjee <117184692+AnanyaMkhj@users.noreply.github.com>
2024-07-03 20:43:57 +0530

    Add files via upload

---
+++ src/quiz/application/Rules.java
@@ -0,0 +1,73 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener {
+    String name;
+    JButton start, back;
+
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        // Get screen dimensions
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+
+        // Set the frame to a size based on the screen size
+        setBounds(50, 50, width - 100, height - 100);
+
+        JLabel heading = new JLabel("Welcome " + name + " to Quiz World");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>" + 
+            "1. Answer point to point." + "<br><br>" +
+            "2. Malpractices will not be tolerated" + "<br><br>" +
+            "3. Don't read the questions aloud." + "<br><br>" +
+            "4. All questions are compulsory." + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
